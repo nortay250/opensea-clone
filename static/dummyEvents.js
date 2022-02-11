d4e5ae3a4a1598f6601da7aecadfb2aebf751d97
nortay250 <eleanortay97@gmail.com>
2022-02-12 01:08:58 +0800

    finished build

---
+++ static/dummyEvents.js
@@ -0,0 +1,20 @@
+export const dummyEvents = [
+    {
+        price: 17,
+        from: "0xnorapps",
+        to: "0xetay009",
+        date: "3 days ago",
+    },
+    {
+        price: 17,
+        from: "0xnorapps",
+        to: "0xetay009",
+        date: "3 days ago",
+    },
+    {
+        price: 17,
+        from: "0xnorapps",
+        to: "0xetay009",
+        date: "3 days ago",
+    }
+]
